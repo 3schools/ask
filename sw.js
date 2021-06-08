2f9e19606726414740aff5810eabc3824dd99f97
3schools <85482920+3schools@users.noreply.github.com>
2021-06-08 13:03:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
       'index.html',
-      'fox-icon.png',
+      'ask.png',
       'index.js',
       'style.css',
       'fox1.jpg',
