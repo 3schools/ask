acaab48a023104e7e1022084c425444b6ee8aca6
3schools <85482920+3schools@users.noreply.github.com>
2021-06-08 13:10:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const images = ['fox1', 'fox2', 'fox3', 'fox4'];
+const images = ['ask', 'ask', 'ask', 'ask'];
 const imgElem = document.querySelector('img');
 
 function randomValueFromArray(array) {
